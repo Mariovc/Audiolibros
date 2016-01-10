6047e934ea7d051292d26d71b7653d491c73d0d7
Mario Velasco <m3ario@gmail.com>
2016-01-10 21:51:23 +0100

    delete book animation

--- app/src/main/java/com/example/audiolibros/fragments/SelectorFragment.java
+++ app/src/main/java/com/example/audiolibros/fragments/SelectorFragment.java
@@ -13,8 +13,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-import com.example.audiolibros.AdaptadorLibros;
 import com.example.audiolibros.AdaptadorLibrosFiltro;
 import com.example.audiolibros.Aplicacion;
 import com.example.audiolibros.Libro;
@@ -29,13 +30,14 @@ import java.util.Vector;
  * Date: 20/12/2015
  * Email: m3ario@gmail.com
  */
-public class SelectorFragment extends Fragment {
+public class SelectorFragment extends Fragment implements Animation.AnimationListener {
     private Activity actividad;
     private RecyclerView recyclerView;
     private AdaptadorLibrosFiltro adaptador;
     private Vector<Libro> vectorLibros;
 
-    @Override public void onAttach(Context contexto) {
+    @Override
+    public void onAttach(Context contexto) {
         super.onAttach(contexto);
         if (contexto instanceof Activity) {
             this.actividad = (Activity) contexto;
@@ -45,13 +47,14 @@ public class SelectorFragment extends Fragment {
         }
     }
 
-    @Override public View onCreateView(LayoutInflater inflador, ViewGroup
+    @Override
+    public View onCreateView(LayoutInflater inflador, ViewGroup
             contenedor, Bundle savedInstanceState) {
         View vista = inflador.inflate(R.layout.fragment_selector,
                 contenedor, false);
         recyclerView = (RecyclerView) vista.findViewById(
                 R.id.recycler_view);
-        recyclerView.setLayoutManager(new GridLayoutManager(actividad,2));
+        recyclerView.setLayoutManager(new GridLayoutManager(actividad, 2));
         recyclerView.setAdapter(adaptador);
         adaptador.setOnItemClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +68,7 @@ public class SelectorFragment extends Fragment {
             public boolean onLongClick(final View v) {
                 final int id = recyclerView.getChildAdapterPosition(v);
                 AlertDialog.Builder menu = new AlertDialog.Builder(actividad);
-                CharSequence[] opciones = { "Compartir", "Borrar ", "Insertar" };
+                CharSequence[] opciones = {"Compartir", "Borrar ", "Insertar"};
                 menu.setItems(opciones, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int opcion) {
                         switch (opcion) {
@@ -82,8 +85,12 @@ public class SelectorFragment extends Fragment {
                                         .setAction("SI", new View.OnClickListener() {
                                             @Override
                                             public void onClick(View view) {
+                                                Animation anim = AnimationUtils.loadAnimation(actividad,
+                                                        R.anim.menguar);
+                                                anim.setAnimationListener(SelectorFragment.this);
+                                                v.startAnimation(anim);
                                                 adaptador.borrar(id);
-                                                adaptador.notifyDataSetChanged();
+                                                //adaptador.notifyDataSetChanged();
                                             }
                                         })
                                         .show();
@@ -94,7 +101,9 @@ public class SelectorFragment extends Fragment {
                                 adaptador.notifyDataSetChanged();
                                 Snackbar.make(v, "Libro insertado", Snackbar.LENGTH_INDEFINITE)
                                         .setAction("OK", new View.OnClickListener() {
-                                            @Override public void onClick(View view) { }
+                                            @Override
+                                            public void onClick(View view) {
+                                            }
                                         })
                                         .show();
                                 break;
@@ -109,4 +118,18 @@ public class SelectorFragment extends Fragment {
     }
 
 
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        adaptador.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
